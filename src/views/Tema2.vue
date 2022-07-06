9d350b883e388ed6b915f2e066766d5465a58225
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-07-06 12:02:51 -0500

    Actualizando

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -9,7 +9,7 @@
 
     .bloque-texto-g.color-1.p-3.p-sm-4.p-md-5.mb-4
       .bloque-texto-g__img(
-        :style="{'background-image': `url(${require('@/assets/curso/tema1/img1.jpg')})`}"
+        :style="{'background-image': `url(${require('@/assets/curso/tema2/img1.jpg')})`}"
       )
       .bloque-texto-g__texto.p-4
         p.mb-0 Para la prestación de los servicios públicos de agua o de alcantarillado, la normativa ambiental colombiana fija unos entes de control que tienen como fin realizar seguimientos y controles a las empresas prestadoras de dichos servicios; de esta manera, se garantiza el cumplimiento de los requisitos mínimos para el uso del recurso hídrico. 
@@ -25,7 +25,7 @@
             .tarjeta-flip__img.bgr_2.align-items-center.p-4.p-xl-5
               figure
                 h5.text-center Comisión de Regulación de Agua Potable y Saneamiento Básico
-                img.img250.p-2.center(src='@/assets/curso/tema1/vec2.svg', alt='imagen 1-2')
+                img.img250.p-2.center(src='@/assets/curso/tema2/vec2.svg', alt='imagen 1-2')
             .tarjeta-flip__contenido.bgr_3.p-4.p-xl-5
               h3.text-center Comisión de Regulación de Agua Potable y Saneamiento Básico
               p.text-center.mt-3 Esta entidad fue creada en la Ley 142 de 1994 como una entidad de orden nacional, quien tiene autonomía administrativa, técnica y patrimonial, y tiene como fin la administración y la vigilancia de los servicios públicos domiciliarios. Su objetivo principal es apoyar la mejora del bienestar de las comunidades a través de la mejora de los servicios públicos de agua, alcantarillado y aseo.
@@ -38,7 +38,7 @@
             .tarjeta-flip__img.bgr_4.align-items-center.p-4.p-xl-5
               figure
                 h3.text-center Corporaciones Autónomas Regionales  - CAR
-                img.img250.p-2.center(src='@/assets/curso/tema1/vec3.svg', alt='imagen 1-2')
+                img.img250.p-2.center(src='@/assets/curso/tema2/vec3.svg', alt='imagen 1-2')
             .tarjeta-flip__contenido.bgr_3.p-4.p-xl-5
               h3.text-center ACorporaciones Autónomas Regionales  - CAR
               p.text-center.mt-3 El objetivo principal de las CAR es velar por la protección del medio ambiente a través del respeto de la normatividad ambiental vigente expedida por el Ministerio de Medio Ambiente. Estas son las encargadas de viabilizar los permisos de usos de agua y los permisos de vertimientos. Las funciones de las CAR están establecidas en la Ley 99 de 1993. 
@@ -47,13 +47,134 @@
     #t_2_1.titulo-segundo.color-acento-contenido
       h2 2.1.	Requisitos para los sistemas de aguas 
 
+    .row.align-items-center
+      .col-md-4
+        figure
+          img(src='@/assets/curso/tema2/img1.jpg', alt='AvatarTop')
+      .col-md-8.mb-2
+        p Para realizar un sistema de acueducto, el primer requisito que se debe tramitar es el permiso de concesión de aguas que consiste en solicitar el derecho de aprovechamiento del agua, ya sea superficial o subterránea para uso doméstico. 
+        .row.mb-4
+          .col-md-2
+            figure
+              img(src='@/assets/curso/tema2/vec1.svg', alt='AvatarTop')
+          .col-md-10
+            p.text-primary Este permiso lo otorga la Corporación Autónoma Regional - CAR a la cual pertenezca el territorio en el que se tiene el proyecto de abastecimiento de agua, después de la evaluación de la solicitud presentada. 
+        p Los requerimientos para la solicitud varían de acuerdo a la corporación territorial a la que se dirija, pero generalmente se tiene en cuenta lo siguiente:
+        
+        .row
+          .col-lg-12
+            TabsA.color-secundario(data-aos="fade")
+              .tarjeta.back-tab.px-5.py-4(titulo="Fuente")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Fuente
+                    p La fuente de captación debe tener un caudal continuo y mayor al caudal solicitado.
+
+                  
+              .tarjeta.back-tab.px-5.py-4(titulo="Propiedad")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Propiedad
+                    p La propiedad del sitio de captación debe contar con los permisos de intervención.
+
+              .tarjeta.back-tab.px-5.py-4(titulo="Descripción del proyecto")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Descripción del proyecto 
+                    p Se debe presentar el proyecto completo y la finalidad de su construcción.
+
+              .tarjeta.back-tab.px-5.py-4(titulo="Costos del proyecto")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Costos del proyecto 
+                    p Este valor es la base del pago de los servicios de valoración de la solicitud.
+
+              .tarjeta.back-tab.px-5.py-4(titulo="Programa de uso eficiente y ahorro de agua ")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Programa de uso eficiente y ahorro de agua 
+                    p Se debe presentar el programa a implementar para la protección del recurso hídrico y de su uso responsable.
+
+              .tarjeta.back-tab.px-5.py-4(titulo="Trámite")
+                .row.px-2.py-2.justify-content-center.align-items-center
+                  .col-lg-12.col-12
+                    h4 Trámite
+                    p Solicitud de acuerdo al formato de la entidad. 
+      br
+      p El formato único nacional de concesión de aguas superficiales es la guía para la presentación de la solicitud, debido a que contiene la guía de diligenciamiento y los documentos anexos de la solicitud. Este formato se puede consultar en el material complementario.       
     separador
 
     #t_2_2.titulo-segundo.color-acento-contenido
       h2 2.2.	Requisitos para vertimientos 
-   
-   
 
+    p.mb-5 Al igual que la solicitud se concesión de aguas, el permiso de vertimientos líquidos también es competencia de la Corporación Autónoma Regional - CAR y el gestor deberá realizar la solicitud de acuerdo con los lineamientos que la territorial exija dentro de su jurisdicción.    
+   
+    .row.mb-5
+      .col-md-12 
+        figure
+          img(src='@/assets/curso/tema2/vec4.svg', alt='AvatarTop')
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-4
+        h5.text-primary Teniendo en cuenta la solicitud, se debe presentar una serie de anexos que varían según la territorial a la que se dirige, pero básicamente son los siguientes:        
+      .col-md-8
+        SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/1.svg' alt='AvatarTop')
+            p.text-center Usos de suelos.
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/2.svg' alt='AvatarTop')
+            p.text-center Permisos de propietarios. 
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/3.svg' alt='AvatarTop')
+            p.text-center Costos del proyecto.  
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/4.svg' alt='AvatarTop')
+            p.text-center Planos y memorias del proyecto. 
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/5.svg' alt='AvatarTop')
+            p.text-center Nombre de la fuente de abastecimiento y de descarga.
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/6.svg' alt='AvatarTop')
+            p.text-center Caracterización del vertimiento.
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/7.svg' alt='AvatarTop')
+            p.text-center Caudal, frecuencia y tiempo de descarga. 
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/8.svg' alt='AvatarTop')
+            p.text-center Ubicación del proyecto.
+
+          .tarjeta.color-adicional-1.p-4
+            .row.justify-content-center.mb-3
+              .col-8
+                img(src='@/assets/curso/tema2/9.svg' alt='AvatarTop')
+            p.text-center Evaluación ambiental del vertimiento.
+
+    p.mb-4 El Formato Único Nacional de Permiso de Vertimientos a Cuerpos de Agua es la guía para la presentación de la solicitud, porque contiene la guía de diligenciamiento y los documentos anexos de la solicitud. Este formato se puede consultar en el material complementario. 
+    
 </template>
 
 <script>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -7,7 +7,22 @@
         span 3
       h1  Estructura de las AAA (Empresas Prestadoras de Servicios Públicos de Acueducto, Alcantarillado y Aseo)
 
-    
+    p.mb-5 Las estructuras de las empresas prestadoras de los servicios públicos de acueducto, alcantarillado y aseo se definen desde su planeación y varían de acuerdo a su tamaño y función. Generalmente la estructura está basada en las áreas y en los departamentos encargados de cada proceso, por ejemplo:
+
+    .row.mb-4.justify-content-center
+      .col-md-6
+        .tarjeta-avatar
+          img.p-2(src='@/assets/curso/tema3/gota1.svg' alt='AvatarTop')
+          .tarjeta.color-1.align-items-center
+            .text-black.p-5
+              p En una empresa pequeña que presta el servicio de suministro de agua a una comunidad, como mínimo, deberá tener un componente directivo y otro operacional.
+
+      .col-md-6
+        .tarjeta-avatar
+          img.p-2(src='@/assets/curso/tema3/gota2.svg' alt='AvatarTop')
+          .tarjeta.color-2.align-items-center
+            .text-black.p-5
+              p En caso de una empresa más grande, además del componente directivo y operacional, deberá tener los componentes de servicio al cliente, financiero, gestión humana, entre otros.
     
 
 </template>
