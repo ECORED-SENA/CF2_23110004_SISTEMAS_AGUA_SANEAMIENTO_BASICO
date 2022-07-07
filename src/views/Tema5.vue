bf4a28cfc495b45768c1aacd3664477cc9a36895
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-07-07 09:10:41 -0500

    Actualizando

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -17,8 +17,37 @@
           p Para que una organización progrese adecuada y sólidamente, es necesario contar con los mecanismos de planificación adecuados. En algunos casos, la tarea de operar y gestionar dichas herramientas y software puede recaer en el equipo de marketing, en el área administrativa o directamente en los miembros fundadores.
           span Sin embargo, para evitar vacíos de información o situaciones que se puedan pasar por alto a la hora de gestionar una empresa, es primordial saber cómo elaborar un plan de acción que se relacione directamente con la actividad de la empresa, para poder proponer acciones que conlleven a una mejora continua.
     
-    p.mb-4 Existen tres elementos a tener en cuenta para el éxito de un plan de acción, los cuales se describen a continuación:
+    p.mb-3 Existen tres elementos a tener en cuenta para el éxito de un plan de acción, los cuales se describen a continuación:
 
+    .row.justify-content-center.p-5.correr-izq.correr-der
+      .col-md-4(data-aos="zoom-in")
+        .crd.crd--gray.crd--hoverBlue.crd--brad.crd--shadow
+          h4.text-center Individuos:
+          p.text-center.p-2 Son organismos unitarios que presentan una estructura independiente, la cual puede ser unicelular o pluricelular y además no tiene la necesidad de otro ser vivo para poder sobrevivir en algún ecosistema.
+          figure
+            img.p-4(src='@/assets/curso/tema5/01.png', alt='AvatarTop' style="width: 190px")
+
+      .col-md-4(data-aos="zoom-in")
+        .crd.crd--gray.crd--hoverBlue.crd--brad.crd--shadow
+          h4.text-center Población:
+          p.text-center.p-2 Esto hace referencia a un grupo de individuos de una misma especie que comparten el mismo espacio y en el mismo tiempo, por lo tanto, presentan diversas relaciones entre ellos, bien sea de alimentación o de reproducción.
+          figure
+            img.p-4(src='@/assets/curso/tema5/02.png', alt='AvatarTop' style="width: 190px")
+
+      .col-md-4(data-aos="zoom-in")
+        .crd.crd--gray.crd--hoverBlue.crd--brad.crd--shadow
+          h4.text-center Comunidad:
+          p.text-center.p-2 Se conoce como el conjunto de varias poblaciones que pueden ser a su vez de diferentes especies pero que habitan en un mismo ecosistema o territorio determinado.
+          figure
+            img.p-4(src='@/assets/curso/tema5/03.png', alt='AvatarTop' style="width: 190px")
+
+    .row 
+      p.mb-4 Con el fin de ampliar los conceptos relacionados con el plan de acción se puede consultar el siguiente video, el cual consolida el concepto. 
+
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Leyenda del video
 
 </template>
 
--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -7,6 +7,80 @@
         span 6
       h1  Evaluación y acciones de mejora 
 
+    p.mb-4 Efectuar procesos de mejora continua no es tan simple. Sin embargo, existen varias estrategias muy efectivas, así como herramientas digitales, para calcular el rendimiento de la actividad empresarial y descubrir qué aspectos se deben corregir.
+    
+    .row.mb-5.justify-content-center.align-items-center
+      .col-md-4
+        h5.mb-4 Como primera medida a realizar, se deben tener en cuenta las siguientes acciones:
+      .col-md-8
+        .row.mb-3 
+          .col-md-4
+            .tarjeta.color-4.p-4
+              .row.justify-content-center.mb-3
+                .col-8
+                  img(src='@/assets/curso/tema6/vec1.svg' alt='AvatarTop')
+              p.text-center Realizar un análisis interno de cada una de las áreas y procesos de la empresa.
+
+          .col-md-4
+            .tarjeta.color-4.p-4
+              .row.justify-content-center.mb-3
+                .col-8
+                  img(src='@/assets/curso/tema6/vec2.svg' alt='AvatarTop')
+              p.text-centerIdear las estrategias de optimización y la elección de herramientas disponibles.
+
+          .col-md-4
+            .tarjeta.color-4.p-4
+              .row.justify-content-center.mb-3
+                .col-8
+                  img(src='@/assets/curso/tema6/vec3.svg' alt='AvatarTop')
+              p.text-centerRevisar el rendimiento para ir canalizando las acciones que están dando resultados positivos y descartar las que no.
+    
+      .row.mb-4.justify-content-center.align-items-center
+        .col-md-7
+          p.mb-2 Además, también será necesario invertir en la digitalización de procesos, mediante herramientas tecnológicas y software, que ayudan a reducir tiempos y esfuerzos, a la vez que aumentan la productividad gracias a la automatización.
+          .row
+            .col-md-12
+              .row.mb-4
+                .col-md-2
+                  figure
+                    img(src='@/assets/curso/tema2/vec1.svg', alt='AvatarTop')
+                .col-md-10
+                  p Existe gran variedad de formas para realizar acciones de mejora, pero en el ámbito empresarial existen tres que se destacan debido a su eficacia. A continuación, se identifican: 
+        .col-md-5
+          figure
+            img(src='@/assets/curso/tema6/img1.jpg', alt='AvatarTop')
+
+      .row.mb-4    
+        AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+          .row(titulo="Establecer los indicadores de cada proceso de la empresa ")
+            .col-md-6.mb-4.mb-md-0
+              p El conocimiento de cada uno de los sectores de la empresa es vital para el establecimiento de los indicadores o instrumentos de medición, estos son los encargados de informar si las metas propuestas están siendo cumplidas o en qué porcentaje de cumplimiento se encuentran.
+              span Para ello, es importante investigar en acciones anteriores y evaluar su eficacia para poder implementarlas nuevamente o si, por el contrario, es necesario establecer nuevos procesos para la actividad.
+
+            .col-md-6
+              figure
+                img(src='@/assets/curso/tema6/img2.jpg', alt='Texto que describa la imagen')
+                figcaption Leyenda de la imagen
+
+          .row(titulo="Planificar las estrategias, establecer las metas y objetivos e identificar qué herramientas digitales serán útiles")
+            .col-md-6.mb-4.mb-md-0
+              p La identificación de las áreas y de los procesos a mejorar es el punto de partida para la fijación de los objetivos y las metas, de esta manera se establecen el horizonte y la ruta a seguir.
+              span Este es un paso importante con el que también se establecen los medios, ya sean digitales u otros que aportarán a la empresa con la medición de los resultados para digitalizar procesos administrativos, reducir recursos y tiempos, eliminar procesos manuales, mejorar los servicios de ventas y cualquier otra necesidad de la empresa.
+
+            .col-md-6
+              figure
+                img(src='@/assets/curso/tema6/img3.jpg', alt='Texto que describa la imagen')
+                figcaption Leyenda de la imagen
+
+          .row(titulo="Analizar los resultados obtenidos y cuantificarlos")
+            .col-md-6.mb-4.mb-md-0
+              p Por último, el análisis de los resultados nos brinda la información necesaria para conocer si los objetivos fueron cumplidos o se deben hacer ajustes para lograrlos.
+              span El seguimiento continuo de todos los procesos nos ayuda a identificar un cambio y establecer nuevamente una estrategia que ayude a cumplir los objetivos de la empresa.
+
+            .col-md-6
+              figure
+                img(src='@/assets/curso/tema6/img4.jpg', alt='Texto que describa la imagen')
+                figcaption Leyenda de la imagen
 
 </template>
 
